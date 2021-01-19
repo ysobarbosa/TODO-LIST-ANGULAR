642dad52f3ff0f2c938ee9d8d21e8bb4691a61f2
Yasmin <ysobarbosa@gmail.com>
2021-01-19 12:04:48 -0300

    production build and connection on firebase hosting

--- TO-DO/src/environments/environment.prod.ts
+++ TO-DO/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC-st06q8aWrMHXrzuuXrUpOv79H6Z-tro',
+    authDomain: 'todo-list-11c4c.firebaseapp.com',
+    databaseURL: 'https://todo-list-11c4c-default-rtdb.firebaseio.com',
+    projectId: 'todo-list-11c4c',
+    storageBucket: 'todo-list-11c4c.appspot.com',
+    messagingSenderId: '542517255594',
+    appId: '1:542517255594:web:254775d54d6b5636231fd3',
+  },
 };
